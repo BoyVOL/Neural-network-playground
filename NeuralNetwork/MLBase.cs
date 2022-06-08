c678d197e8b6d4194c32ac5feeb0e36ea0205ee6
BoyVOL <vladislav_limonov@mail.ru>
2022-06-08 17:17:27 +0900

    1

--- NeuralNetwork/MLBase.cs
+++ NeuralNetwork/MLBase.cs
@@ -8,37 +8,121 @@ namespace DeepLearning
     public class NNLayer{
 
         /// <summary>
-        /// Количество входов слоя
-        /// </summary>
-        public readonly int InputSize;
-
-        /// <summary>
-        /// Количество выходов слоя
-        /// </summary>
-        public readonly int OutputSize;
-
-        /// <summary>
         /// Веса связей между нейронами предыдущего и данного слоя.
         /// </summary>
         protected float[,] Weights;
 
         /// <summary>
-        /// Конструктор класса
+        /// Буфер для выхода нейронного слоя
+        /// </summary>
+        protected float[] Output;
+
+        /// <summary>
+        /// Инициализация слоя
         /// </summary>
         /// <param name="inputSize">Количество входов</param>
         /// <param name="outputSize">Количество выходов</param>
         public NNLayer(int inputSize, int outputSize){
-            InputSize = inputSize;
-            OutputSize = outputSize;
             Weights = new float[inputSize,outputSize];
-            for (int i = 0; i < inputSize; i++)
+            Output = new float[outputSize];
+            ResetWeights();
+        }
+
+        /// <summary>
+        /// Функция для вычисления ответа нейрона в соответствии со входным значением
+        /// </summary>
+        /// <param name="x">вход функции</param>
+        /// <returns></returns>
+        public virtual float SigmoidFunction(float x){
+            return (float)(1/(1+Math.Exp(-x)));
+        }
+
+        /// <summary>
+        /// Метод, возвращающий число входов слоя
+        /// </summary>
+        /// <returns></returns>
+        public int GetInputSize(){
+            return Weights.GetLength(0);
+        }
+
+        /// <summary>
+        /// Метод, возвращающий количество выходов слоя
+        /// </summary>
+        /// <returns></returns>
+        public int GetOutputSize(){
+            return Weights.GetLength(1);
+        }
+
+        /// <summary>
+        /// Метод для переопределения стартовых весов слоя.
+        /// </summary>
+        /// <returns></returns>
+        public virtual void ResetWeights(){
+            for (int inp = 0; inp < GetInputSize(); inp++)
             {
-                for (int j = 0; j < OutputSize; j++)
+                for (int outp = 0; outp < GetOutputSize(); outp++)
                 {
-                    Weights[i,j]=0;
+                    Weights[inp,outp]= 0;
                 }
             }
         }
+
+        /// <summary>
+        /// Метод для переопределения стартовых весов слоя.
+        /// </summary>
+        /// <returns></returns>
+        public virtual void ResetWeights(Random rand){
+            for (int inp = 0; inp < GetInputSize(); inp++)
+            {
+                for (int outp = 0; outp < GetOutputSize(); outp++)
+                {
+                    Weights[inp,outp]= (float)(rand.NextDouble()-0.5);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод, вычисляющий выход одного нейрона
+        /// </summary>
+        /// <param name="input">массив входа нейрона</param>
+        /// <param name="id">индекс нейрона в матрице весов</param>
+        /// <returns></returns>
+        protected float SingleNeuronOutput(float[] input, int id){            
+                float value = 0;
+                //для каждого выходного нейрона собираем сумму его входов, помноженных на вес слоя
+                for (int inp = 0; inp < GetInputSize(); inp++)
+                {
+                    value += input[inp]*Weights[inp,id];
+                }
+                return SigmoidFunction(value);
+        }
+
+        /// <summary>
+        /// Метод, генерирующий выходной массив значений слоя
+        /// </summary>
+        /// <param name="input"></param>
+        public void GenerateOutput(float[] input){
+            if(input.Length != GetInputSize())
+                throw new Exception("input array size does not match array size. Input = "+input.Length+", Layer input = "+GetInputSize());
+            //обновляем массив выходов слоя
+            Output = new float[GetOutputSize()];
+            //проходим по всем выходным нейронам
+            for (int outp = 0; outp < GetOutputSize(); outp++)
+            {
+                Output[outp] = SingleNeuronOutput(input, outp);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Класс, отображающий целую нейросеть
+    /// </summary>
+    public class NeuralNetwork{
+
+        /// <summary>
+        /// Массив 
+        /// </summary>
+        public NNLayer[] Layers;
 
         /// <summary>
         /// Вычисление входа по каждой "лапке" логистического нейрона
@@ -50,17 +134,5 @@ namespace DeepLearning
         public virtual float SingleLogInput(float w, float x, float b = 0){
             return w*x-b;
         }
-
-        /// <summary>
-        /// Функция для вычисления ответа нейрона в соответствии со входным значением
-        /// </summary>
-        /// <param name="x">вход функции</param>
-        /// <returns></returns>
-        public virtual double SigmoidFunction(float x){
-            return 1/(1+Math.Exp(-x));
-        }
-
-
     }
-
 }
