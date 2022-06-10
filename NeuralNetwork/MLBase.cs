1cb3eec18c096d956e3954688ffefaba3aa47975
BoyVOL <vladislav_limonov@mail.ru>
2022-06-10 17:30:32 +0900

    1

--- NeuralNetwork/MLBase.cs
+++ NeuralNetwork/MLBase.cs
@@ -1,4 +1,5 @@
 using System;
+using Godot;
 
 namespace DeepLearning
 {
@@ -11,6 +12,11 @@ namespace DeepLearning
         /// Веса связей между нейронами предыдущего и данного слоя.
         /// </summary>
         protected float[,] Weights;
+
+        /// <summary>
+        /// массив, хранящий предыдущие исправления весов
+        /// </summary>
+        protected float[,] PrevCorr;
 
         /// <summary>
         /// Буфер для выхода нейронного слоя
@@ -32,11 +38,17 @@ namespace DeepLearning
         /// </summary>
         /// <param name="inputSize">Количество входов</param>
         /// <param name="outputSize">Количество выходов</param>
-        public NNLayer(int inputSize, int outputSize){
+        public NNLayer(int inputSize, int outputSize, Random rand = null){
             Weights = new float[inputSize,outputSize];
+            PrevCorr = new float[inputSize,outputSize];
+            Input = new float[inputSize];
             Output = new float[outputSize];
             Error = new float[outputSize];
-            ResetWeights();
+            if(rand!=null){
+                ResetWeights(rand);
+            } else {
+                ResetWeights();
+            }
         }
 
         /// <summary>
@@ -44,7 +56,7 @@ namespace DeepLearning
         /// </summary>
         /// <param name="x">вход функции</param>
         /// <returns></returns>
-        public virtual float AcivationFunct(float x){
+        protected virtual float AcivationFunct(float x){
             return (float)(1/(1+Math.Exp(-x)));
         }
 
@@ -53,7 +65,7 @@ namespace DeepLearning
         /// </summary>
         /// <param name="x">значение функции</param>
         /// <returns></returns>
-        public virtual float AcivationDeriv(float x){
+        protected virtual float AcivationDeriv(float x){
             return x*(1-x);
         }
 
@@ -73,6 +85,14 @@ namespace DeepLearning
             return Weights.GetLength(1);
         }
 
+        /// <summary>
+        /// Возвращает массив выхода
+        /// </summary>
+        /// <returns></returns>
+        public float[] GetOutput(){
+            return Output;
+        }
+        
         /// <summary>
         /// Метод для переопределения стартовых весов слоя.
         /// </summary>
@@ -96,7 +116,7 @@ namespace DeepLearning
             {
                 for (int outp = 0; outp < GetOutputSize(); outp++)
                 {
-                    Weights[inp,outp]= (float)(rand.NextDouble()-0.5);
+                    Weights[inp,outp]= (float)(rand.NextDouble()*2-1);
                 }
             }
         }
@@ -147,7 +167,8 @@ namespace DeepLearning
         /// </summary>
         /// <param name="id">индекс нейрона</param>
         /// <param name="speed">коэффициент изменения весов</param>
-        protected void AdjustWSingle(int id, float speed){
+        /// <param name="InCoeff">коэффициент инерции изменений</param>
+        protected void AdjustWSingle(int id, float speed, float InCoeff){
             for (int inp = 0; inp < GetInputSize(); inp++)
             {
                 Weights[inp,id] -= speed*Error[id]*Input[inp];
@@ -155,13 +176,14 @@ namespace DeepLearning
         }
 
         /// <summary>
-        /// Метод для корректировки весов всех нейронов слоя
+        /// Метод для корректировки весов всех нейронов слоя. для корректировки должна быть задана ошибка
         /// </summary>
         /// <param name="speed">коэффициент изменения весов</param>
-        public void AdjustW(float speed){
+        /// <param name="InCoeff">коэффициент инерции изменений</param>
+        public void AdjustW(float speed, float InCoeff){
             for (int i = 0; i < GetOutputSize(); i++)
             {
-                AdjustWSingle(i,speed);
+                AdjustWSingle(i,speed,InCoeff);
             }
         }
 
@@ -172,13 +194,13 @@ namespace DeepLearning
         /// <param name="NextWeights">веса следующего слоя</param>
         /// <param name="NextErrors">Ошибки следующего слоя</param>
         /// <returns></returns>
-        protected float WeightedErrorOfNeuron(int id, float[,]NextWeights, float[] NextErrors){
-            if(NextWeights.GetLength(1) != NextErrors.Length)
-                throw new Exception("Output size of passed weight matrix does not match with errors size. Weight Matr = "+NextWeights.GetLength(1)+", Errors = "+NextErrors.Length);
+        protected float WeightedErrorOfNeuron(int id, NNLayer Next){
+            if(Next.GetOutputSize() != Next.Error.Length)
+                throw new Exception("Output size of passed weight matrix does not match with errors size. Weight Matr = "+Next.GetOutputSize()+", Errors = "+ Next.Error.Length);
             float Result = 0;
-            for (int i = 0; i < NextWeights.GetLength(1); i++)
+            for (int i = 0; i < Next.GetOutputSize(); i++)
             {
-                Result += NextWeights[id,i]+NextErrors[i];
+                Result += Next.Weights[id,i]*Next.Error[i];
             }
             return Result;
         }
@@ -197,11 +219,77 @@ namespace DeepLearning
         /// Метод для заполнения всего массива ошибок слоя
         /// </summary>
         /// <param name="NextErrors"></param>
-        public void FillErrors(float[,]NextWeights, float[] NextErrors){
+        public void FillErrors(NNLayer Next){
             for (int i = 0; i < GetOutputSize(); i++)
             {
-                CalculateErrorSingle(WeightedErrorOfNeuron(i,NextWeights,NextErrors),i);
+                CalculateErrorSingle(WeightedErrorOfNeuron(i,Next),i);
             }
+        }
+
+        /// <summary>
+        /// Метод для заполнения массива ошибок слоя. Перегрузка для заданных изначально значений массива
+        /// </summary>
+        public void FillErrors(float[]ErrorVals){   
+            if(GetOutputSize() != ErrorVals.Length)
+                throw new Exception("ErrorVal size of does not match with output size. Oputput size = "+GetOutputSize()+", Error size = "+ErrorVals.Length);
+                     
+            for (int i = 0; i < GetOutputSize(); i++)
+            {
+                CalculateErrorSingle(ErrorVals[i],i);
+            }
+        }
+
+        /// <summary>
+        /// Метод для строкового представления весов слоя
+        /// </summary>
+        /// <returns></returns>
+        public string WeightsToString(){
+            string result = "";
+            for (int i = 0; i < GetInputSize(); i++)
+            {
+                for (int j = 0; j < GetOutputSize(); j++)
+                {
+                    result += Weights[i,j]+" ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+    
+        public string InputToString(){
+            string result = "";
+                for (int j = 0; j < GetInputSize(); j++)
+                {
+                    result += Input[j]+" ";
+                }
+            return result;
+        }
+    
+        public string OutputToString(){
+            string result = "";
+                for (int j = 0; j < GetOutputSize(); j++)
+                {
+                    result += Output[j]+" ";
+                }
+            return result;
+        }
+    
+        public string ErrorToString(){
+            string result = "";
+                for (int j = 0; j < GetOutputSize(); j++)
+                {
+                    result += Error[j]+" ";
+                }
+            return result;
+        }
+    
+        public string StateToString(){
+            string result = "";
+            result += "Weights = "+WeightsToString();
+            result += "Input = "+InputToString();
+            result += "Output = "+OutputToString();
+            result += "Error = "+ErrorToString();
+            return result;
         }
     }
 
--- Node2D.cs
+++ Node2D.cs
@@ -4,6 +4,24 @@ using DeepLearning;
 
 public class Node2D : Godot.Node2D
 {
+    Random Rand = new Random();
+    NNLayer TestLayer;
+    NNLayer TestLayer2;
+
+    /// <summary>
+    /// Тестовая функция, которая заставляет слой нейросети выдавать тот же массив, что подавался на вход
+    /// </summary>
+    public void LearnEcho(float [] Array,float[] Expected){
+        TestLayer.AdjustW(1,0.5f);
+        TestLayer2.AdjustW(1,0.5f);
+        TestLayer.SetInput(Array);
+        TestLayer.GenerateOutput();
+        TestLayer2.SetInput(TestLayer.GetOutput());
+        TestLayer2.GenerateOutput();
+        float[] Error = new float[] {TestLayer2.GetOutput()[0]-Expected[0],TestLayer2.GetOutput()[1]-Expected[1]};
+        TestLayer2.FillErrors(Error);
+        TestLayer.FillErrors(TestLayer2);
+    }
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -11,7 +29,29 @@ public class Node2D : Godot.Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        TestLayer = new NNLayer(2,4,Rand);
+        TestLayer2 = new NNLayer(4,2,Rand);
         GD.Print("HelloWorld");
+        for (int i = 0; i < 1000; i++)
+        {
+            GD.Print(i);
+            LearnEcho(new float[] {0f,1f},new float[] {1,0});
+            LearnEcho(new float[] {1,0},new float[] {1,0});
+            LearnEcho(new float[] {1,1},new float[] {0,1});
+            LearnEcho(new float[] {0,0},new float[] {0,1});
+        }
+            LearnEcho(new float[] {0f,1f},new float[] {1,0});
+        GD.Print("Layer1 = ",TestLayer.StateToString());
+        GD.Print("Layer2 = ",TestLayer2.StateToString());
+            LearnEcho(new float[] {1,0},new float[] {1,0});
+        GD.Print("Layer1 = ",TestLayer.StateToString());
+        GD.Print("Layer2 = ",TestLayer2.StateToString());
+            LearnEcho(new float[] {1,1},new float[] {0,1});
+        GD.Print("Layer1 = ",TestLayer.StateToString());
+        GD.Print("Layer2 = ",TestLayer2.StateToString());
+            LearnEcho(new float[] {0,0},new float[] {0,1});
+        GD.Print("Layer1 = ",TestLayer.StateToString());
+        GD.Print("Layer2 = ",TestLayer2.StateToString());
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
